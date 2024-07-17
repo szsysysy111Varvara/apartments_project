5008c39151c8ed0cb01d38907995ecfd4a39dcf9
VarvaraOliinyk <barbaraoliinyk@gmail.com>
2024-07-17 19:52:56 +0200

    Create base for project

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rentapp',
+    'users',
     'django_filters',
     'rest_framework',
 ]
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from users import views
+from users.views import UserRegistrationAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rentapp/', include('rentapp.urls')),
+    path('users/', include('users.urls')),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', UserRegistrationAPIView.as_view(), name='register'),
 ]
