117d50a097063222576556fab3fad6fc36ba87e1
VarvaraOliinyk <barbaraoliinyk@gmail.com>
2024-07-19 05:22:08 +0200

    Create superuser

--- rentapp/admin.py
+++ rentapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from rentapp.models import Listing
+from rentapp.models import Booking
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'listing', 'owner', 'renter', 'start_date', 'end_date', 'status')
+    list_filter = ('status', 'start_date', 'end_date')
+    search_fields = ('listing__name', 'owner__username', 'renter__username')
+    date_hierarchy = 'start_date'
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'owner', 'price', 'availability')
+    list_filter = ('availability',)
+    search_fields = ('name', 'owner__username')
+    date_hierarchy = 'created_at'
+
+admin.site.register(Listing)
+admin.site.register(Booking)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_type', 'phone_number')
+    list_filter = ('user_type',)
+    search_fields = ('user__username')
+
+admin.site.register(Profile)
